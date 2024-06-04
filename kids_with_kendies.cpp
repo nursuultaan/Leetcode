89448f4bb3aca4e7971f73409723f1f9f823afab
Nursultan <nursultan_kas@icloud.com>
2024-06-04 00:17:51 -0400

    leetcode

--- kids_with_kendies.cpp
+++ kids_with_kendies.cpp
@@ -1,3 +1,17 @@
+#include "vector"
+#include "iostream"
+using namespace std;
+
+
+vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+}
+
+
 int main(){
+    vector<int> v = {2,3,5,1,3};
+    int num_kendies = 3;
+
+
     return 0;
 }

2ed999b81fad3cf601865881c0b27b5b65f149ac
Nursultan <nursultan_kas@icloud.com>
2024-06-04 00:50:47 -0400

    leetcode

--- flowers_adjesent.cpp
+++ flowers_adjesent.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 bool canPlaceFlowers(vector<int>& flowerbed, int n) {
 
+    bool flower_warning = false;
+
+    for(int i = 0;i<flowerbed.size();i++){
+        if(flowerbed[i] == 1 && !flower_warning){
+            i+= n;
+            flower_warning = true;
+        }
+        if(flower_warning){
+            for(int j = i;j<=i+n;j++){
+                if(flowerbed[j] == 1){
+                    return false;
+                }
+            }
+            flower_warning = false;
+        }
+
+
+
+    }
+    return true;
+
+
 }
 
 
@@ -12,7 +34,7 @@ int main(){
 
     std::vector<int> v = {1,0,0,0,1};
     int n = 1;
-    canPlaceFlowers(v,n);
+    std::cout<<canPlaceFlowers(v,n);
 
 
 }

961e1a860e6f085de7e8edc4177d61b45eecb2bb
Nursultan <nursultan_kas@icloud.com>
2024-06-04 00:29:46 -0400

    leetcode

---
+++ flowers_adjesent.cpp
@@ -0,0 +1,18 @@
+
+#include "vector"
+#include "iostream"
+using namespace std;
+
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+}
+
+
+int main(){
+
+    std::vector<int> v = {1,0,0,0,1};
+    int n = 1;
+    canPlaceFlowers(v,n);
+
+
+}

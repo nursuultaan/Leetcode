510cd70b6d83fa4418499cc02eb1bb16d6c34f6c
Nursultan <nursultan_kas@icloud.com>
2024-06-04 00:16:04 -0400

    leetcode

--- string_greatest_common_divisor.cpp
+++ string_greatest_common_divisor.cpp
@@ -6,8 +6,7 @@ using  namespace std;
 string gcdOfStrings(string str1, string str2) {
     string  res ;
     string divisor = str1.size() < str2.size() ? str2 : str1;
-    string divider = str1.size() > str2.size() ? str2 : str1;
-
+    string divider = str1.size() >= str2.size() ? str2 : str1;
 
     size_t pointer_divisor = 0;
     size_t pointer_divider = 0;
@@ -17,9 +16,15 @@ string gcdOfStrings(string str1, string str2) {
     bool isDividable = true;
 
     while (pointer_divisor < divisor.size()){
-        if(divisor[pointer_divisor] ==  divider[pointer_divider]){
-            res.push_back(divider[pointer_divider]);
-            res_right++;
+        if(divisor[pointer_divisor] ==  divider[pointer_divider] ){
+            string t = divisor.substr(res_right ,res_right   );
+            if(divisor.substr(res_right,res_right ) == res && !res.empty()){
+
+            } else{
+                res.push_back(divider[pointer_divider]);
+                res_right++;
+            }
+
         }else{
             if((divisor.substr(res_left,res_right) == res)){
                 isDividable = true;
@@ -33,8 +38,10 @@ string gcdOfStrings(string str1, string str2) {
         pointer_divisor++;
         pointer_divider++;
         if(pointer_divider >= divider.size() ){
-            if(pointer_divider == divider.size()){
-                return res;
+            if(pointer_divider == divider.size() && !res.empty()){
+                if(divisor[divisor.size() -1] == res[res.size() -1]) return res;
+                else return "";
+
             }
 
            pointer_divider=0;
@@ -44,13 +51,14 @@ string gcdOfStrings(string str1, string str2) {
 
     }
 
+
     return res;
 }
 
 
 int main(){
-    string word1 = "ABABAB";
-    string word2 = "ABAB";
+    string word1 = "ABCDEF";
+    string word2 = "ABC";
 
     std::cout<<gcdOfStrings(word1,word2);
 }

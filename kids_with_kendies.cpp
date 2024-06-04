8806be5d77e1e61f327481c5e0dbfd820cc681af
Nursultan <nursultan_kas@icloud.com>
2024-06-04 00:20:53 -0400

    leetcode

--- kids_with_kendies.cpp
+++ kids_with_kendies.cpp
@@ -4,13 +4,24 @@ using namespace std;
 
 
 vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    int max = -1;
+    for(int i = 0;i<candies.size();i++){
+        if(max<candies[i]){
+            max = candies[i];
+        }
+    }
+    std::cout<<max<<"\n";
 
+    for(int kid : candies){
+
+    }
 }
 
 
 int main(){
     vector<int> v = {2,3,5,1,3};
     int num_kendies = 3;
+    kidsWithCandies(v,num_kendies);
 
 
     return 0;
